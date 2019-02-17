a2be9bc0ca09ea8df1a61b38244d6755c6442f99
Alex Dresco <a.dresco@elsevier.com>
2019-02-17 12:19:40 +0000

    Added React Bootstrap and formatted the navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
-class Navigation extends React.Component {
+export default class Navigation extends React.Component {
   render(){
     return(
-    <div id='nav-bar'>
-      <h1>Amina Bashir Memorial Trust</h1>
-      <nav>
-        <a href="/">Home</a>
-        <a href="/contact">Contact</a>
-        <a href="/photos"> Photos </a>
-        <a href="/facilities"> Facilities </a>
-      </nav>
-    </div>)
+    <div>
+      <Navbar bg="light" expand="md" variant="light" fixed="top">
+        <Navbar.Brand>Amina Bashir Memorial Trust</Navbar.Brand>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+              <Nav.Link href="/photos"> Photos </Nav.Link>
+              <Nav.Link href="/facilities"> Facilities </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+      </Navbar>
+    </div>
+  )
   }
 }
-
-export default Navigation
