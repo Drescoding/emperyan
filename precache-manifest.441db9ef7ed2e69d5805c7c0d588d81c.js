3a44bf5459db0e862931bb0476d433218ce68429
Alex Dresco <a.dresco@elsevier.com>
2019-04-08 20:32:12 +0100

    Updates

---
+++ precache-manifest.441db9ef7ed2e69d5805c7c0d588d81c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3521028a73c808b3376",
+    "url": "/my-app/static/css/main.3ddad89d.chunk.css"
+  },
+  {
+    "revision": "e3521028a73c808b3376",
+    "url": "/my-app/static/js/main.e3521028.chunk.js"
+  },
+  {
+    "revision": "e62dbed6ac469c8cdf5e",
+    "url": "/my-app/static/js/1.e62dbed6.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "0c4974df161abb5ef6fd6838eaa6ae12",
+    "url": "/my-app/static/media/homeImage.0c4974df.jpg"
+  },
+  {
+    "revision": "8bee22d6e02950914c704b0f4891b942",
+    "url": "/my-app/index.html"
+  }
+];
