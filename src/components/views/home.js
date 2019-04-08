6fd0a50a50abd9532e923b636dab675caad0711b
Alex Dresco <a.dresco@elsevier.com>
2019-04-08 18:29:12 +0100

    Created pages

--- src/components/views/home.js
+++ src/components/views/home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { browserHistory } from 'react-router';
 import Navigation from '../Navigation.js';
+import homeImage from './homeImage.jpg'; 
 
 export default class Home extends Component {
   componentDidMount() {
@@ -10,12 +11,15 @@ export default class Home extends Component {
     return (
       <div id="home">
         <Navigation />
-
-        <h1>Introducing Empyrean Trust</h1>
-
-        <p>At Empyrean Trust, we want to help companies that help others succeed. We’re a business incubator and accelerator offering a full spectrum of funding, support, and consulting services to companies that have a strong social or generative mission. We are uniquely well-equipped to support owners and founders from demographics that often face structural or economic barriers to upward mobility: women, racial minorities, veterans, those with disabilities, poor rural populations, and LGBT people.
-
-        Let us help you make the world a better place.</p>
+        <div class="container">
+        <img src={homeImage} alt='background' id='background'/>
+          <div class="centered">
+            <p>OUR MISSION</p>
+            <h1>Empower.</h1>
+            <p>We ensure social enterprises can thrive and prosper by providing 
+              them a platform and tool kits to achieve their growth strategies.</p>
+          </div>
+        </div>
       </div>
 
     );
