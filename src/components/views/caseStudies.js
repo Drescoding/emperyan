86635391e00e28c82e592dcdf695953def687a0e
Alex Dresco <a.dresco@elsevier.com>
2019-05-18 16:47:31 +0100

    heroku

--- src/components/views/caseStudies.js
+++ src/components/views/caseStudies.js
@@ -1,22 +1,58 @@
 import React, { Component } from "react";
 import Navigation from '../Navigation.js';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
 
 export default class CaseStudies extends Component {
+  state = {
+    open: false,
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
   render() {
     return (
       <div>
         <Navigation />
         <GridList cellHeight={160} className={styles.gridList} cols={3}>
-        {tileData.map(tile => (
-          <GridListTile key={tile.img} cols={tile.cols || 1}>
-            <img src={tile.img} alt={tile.title} />
-          </GridListTile>
-        ))}
-      </GridList>
+          {tileData.map(tile => (
+            <GridListTile key={tile.img} cols={tile.cols || 1} onClick={this.handleClickOpen}>
+              <img src={tile.img} alt={tile.title} />
+            </GridListTile>
+          ))}
+        </GridList>
+
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Project 1</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Ex sint nobis invenire nam. Usu et delectus urbanitas. An munere dignissim sed, quis soluta alienum vim an. An officiis aliquando eum. Est ea offendit suscipit appetere, cum in aeterno ponderum sapientem, ei has quas suscipiantur deterruisset.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Close
+            </Button>
+          </DialogActions>
+
+        </Dialog>
       </div>
     );
   }
