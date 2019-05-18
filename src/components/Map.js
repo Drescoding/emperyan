2eb888253e7a807467fd7c19596e7885529dd1a8
Alex Dresco <a.dresco@elsevier.com>
2019-05-18 22:53:03 +0100

    markers

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+const Marker = ({ text }) => <div>
+  <SvgIcon>
+    <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+  </SvgIcon>
+</div>;
 
 class SimpleMap extends Component {
   static defaultProps = {
@@ -15,15 +21,27 @@ class SimpleMap extends Component {
   render() {
     return (
       // Important! Always set the container height explicitly
-      <div id="map" style={{ height: '15vh', width: '80%' }}>
+      <div id="map" style={{ height: '25vh', width: '80%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyAMM_IDcrfGURCPQQjmzDpStMBopgEckec' }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <AnyReactComponent
+          <Marker
             lat={40.955413}
             lng={-73.337844}
+            text="My Marker"
+          />
+
+          <Marker
+            lat={42.955413}
+            lng={-74.337844}
+            text="My Marker"
+          />
+
+          <Marker
+            lat={41.955413}
+            lng={-76.337844}
             text="My Marker"
           />
         </GoogleMapReact>
