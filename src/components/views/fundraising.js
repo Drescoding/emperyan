d61040e59390e87434c39af5e5ac00e4160ae656
Alex Dresco <a.dresco@elsevier.com>
2019-05-19 18:34:26 +0100

    fundraising page

--- src/components/views/fundraising.js
+++ src/components/views/fundraising.js
@@ -7,29 +7,31 @@ export default class Fundraising extends Component {
     return (
       <div className="content" onScrollCapture>
         <Navigation />
-        <h1>Fundraising</h1>
-        <div style={{ display: "flex", flexWrap: "wrap" }}>
+        <div className="whiteBox">
+          <h1>Fundraising</h1>
+          <p>Lorem ipsum dolor sit amet, eripuit vituperata in pri. Graeci regione dolorem vel ut, id alia scaevola mea, vel errem graeci ea. At est mundi principes, an dicunt sententiae duo, eros dissentiunt in pro. Has an idque assum recteque, ei mandamus aliquando has, dolor fabulas torquatos eu vis. Hinc dolor eos ex, at similique moderatius has.</p>
+          <p>Let us help you make the world a better place.</p>
+        </div>
         <VictoryChart
-            theme={VictoryTheme.material}
-            domainPadding={20}
-            style={{ parent: { maxWidth: "55%" } }}
-          >
-            <VictoryAxis
-              tickValues={[1, 2, 3, 4, 5]}
-              tickFormat={["2014", "2015", "2016", "2017","2018"]}
-            />
-            <VictoryAxis
-              dependentAxis
-              tickFormat={(x) => (`$${x / 1000000}M`)}
-            />
-            <VictoryBar
-              data={data}
-              x="year"
-              y="earnings"
-            />
-          </VictoryChart>
-          <div>The following is placeholder text known as “lorem ipsum,” which is scrambled Latin used by designers to mimic real copy. Vivamus a ante congue, porta nunc nec, hendrerit turpis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae. Integer tempus, elit in laoreet posuere, lectus neque blandit dui, et placerat urna diam mattis orci.</div>
-          </div>
+          theme={VictoryTheme.material}
+          domainPadding={20}
+          style={{ parent: { maxWidth: "55%" } }}
+          animate={{ duration: 500 }}
+        >
+          <VictoryAxis
+            tickValues={[1, 2, 3, 4, 5]}
+            tickFormat={["2014", "2015", "2016", "2017", "2018"]}
+          />
+          <VictoryAxis
+            dependentAxis
+            tickFormat={(x) => (`$${x / 1000000}M`)}
+          />
+          <VictoryBar
+            data={data}
+            x="year"
+            y="earnings"
+          />
+        </VictoryChart>
       </div>
     );
   }
