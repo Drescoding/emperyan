f6efd6b3ea88da6e870c022e522765cdc18cac20
Alex Dresco <a.dresco@elsevier.com>
2019-06-03 21:18:26 +0100

    space around map to remove

--- src/components/views/home.js
+++ src/components/views/home.js
@@ -8,95 +8,94 @@ import Team from './team.js'
 // import logo from './logo.png'
 
 export default class Home extends Component {
-  componentDidMount() {
-    browserHistory.push('/');
-  }
-  render() {
-    return (
-      <div>
-        <Navigation />
-        {/* <div id="backgroundImage">
-          <img id="centered_image" src={logo} alt="logo" />
-        </div> */}
-        <div id="backgroundImage" className="centered">
-          <p>OUR MISSION:</p>
-          <h1>Empower.</h1>
-          <p>We ensure social enterprises can thrive and prosper by providing
-              them a platform and tool kits to achieve their growth strategies.</p>
-        </div>
+componentDidMount() {
+  browserHistory.push('/');
+}
+render() {
+  return (
+    <div>
+      <Navigation />
+      {/* <div id="backgroundImage">
+        <img id="centered_image" src={logo} alt="logo" />
+      </div> */}
+      <div id="backgroundImage" className="centered">
+        <p>OUR MISSION:</p>
+        <h1>Empower.</h1>
+        <p>We ensure social enterprises can thrive and prosper by providing
+            them a platform and tool kits to achieve their growth strategies.</p>
+      </div>
 
+      <div className="content">
+        {/* <div className="whiteBox"> */}
+        <h1>Our philosophy</h1>
+        <p>Lorem ipsum dolor sit amet, eripuit vituperata in pri. Graeci regione dolorem vel ut, id alia scaevola mea, vel errem graeci ea. At est mundi principes, an dicunt sententiae duo, eros dissentiunt in pro. Has an idque assum recteque, ei mandamus aliquando has, dolor fabulas torquatos eu vis. Hinc dolor eos ex, at similique moderatius has.</p>
+        <p>Let us help you make the world a better place.</p>
+        {/* </div> */}
+
+        <div className="whiteBoxButton">
+          <a className="button" href="/growth_hacking">Growth Hacking</a>
+          <a className="button" href="/fundraising">Fundraising</a>
+        </div>
+      </div>
+
+      {/* Refactor this */}
+      <div className="background">
+        <div className="content_black">
         <div className="content">
-          {/* <div className="whiteBox"> */}
-          <h1>Our philosophy</h1>
-          <p>Lorem ipsum dolor sit amet, eripuit vituperata in pri. Graeci regione dolorem vel ut, id alia scaevola mea, vel errem graeci ea. At est mundi principes, an dicunt sententiae duo, eros dissentiunt in pro. Has an idque assum recteque, ei mandamus aliquando has, dolor fabulas torquatos eu vis. Hinc dolor eos ex, at similique moderatius has.</p>
-          <p>Let us help you make the world a better place.</p>
-          {/* </div> */}
-
-          <div className="whiteBoxButton">
-            <a className="button" href="/growth_hacking">Growth Hacking</a>
-            <a className="button" href="/fundraising">Fundraising</a>
+            <h1>Our Global Reach</h1>
           </div>
+            <Map />
         </div>
+      </div>
 
-        {/* Refactor this */}
-        <div className="background">
-          <div className="content_black">
-            <div className="map">
-              <h1>Our Global Reach</h1>
-              <br />
-              <Map />
-            </div>
-          </div>
+      <Team />
+
+      <div className="background">
+        <div className="content">
+          <h1>Investments</h1>
+          <GridList cols={3}>
+            {tileData.map(tile => (
+              <GridListTile key={tile.img}>
+                <img src={tile.img} alt={tile.title} />
+              </GridListTile>
+            ))}
+          </GridList>
         </div>
-
-        <Team />
-
-        <div className="background">
-          <div className="content_black">
-            <h1>Investments</h1>
-            <GridList cols={3}>
-              {tileData.map(tile => (
-                <GridListTile key={tile.img}>
-                  <img src={tile.img} alt={tile.title} />
-                </GridListTile>
-              ))}
-            </GridList>
-          </div>
-        </div>
-      </div >
-    );
-  }
+      </div>
+    </div >
+  );
+}
 }
 
 const tileData = [
 
-  {
-    img: 'https://graphiste.com/blog/wp-content/uploads/2016/07/startup-2.png',
-    title: 'Mint',
-    description: 'This is what we did for the project',
-    cols: 1,
-  },
-  {
-    img: 'https://graphiste.com/blog/wp-content/uploads/2016/07/startup-1.png',
-    title: 'DocDuck',
-    description: 'This is what we did for the project',
-    cols: 1,
-  },
-  {
-    img: 'https://graphiste.com/blog/wp-content/uploads/2016/07/startup-4.png',
-    title: 'DocDuck',
-    description: 'This is what we did for the project',
-    cols: 1,
-  }
+{
+  img: 'https://graphiste.com/blog/wp-content/uploads/2016/07/startup-2.png',
+  title: 'Mint',
+  description: 'This is what we did for the project',
+  cols: 1,
+},
+{
+  img: 'https://graphiste.com/blog/wp-content/uploads/2016/07/startup-1.png',
+  title: 'DocDuck',
+  description: 'This is what we did for the project',
+  cols: 1,
+},
+{
+  img: 'https://graphiste.com/blog/wp-content/uploads/2016/07/startup-4.png',
+  title: 'DocDuck',
+  description: 'This is what we did for the project',
+  cols: 1,
+}
 ]
 
 const styles = theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
+root: {
+  flexGrow: 1,
+},
+paper: {
+  padding: theme.spacing.unit * 2,
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+},
 });
