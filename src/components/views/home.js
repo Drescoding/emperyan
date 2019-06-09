80f68bd59f9a0270718fee526476262ce8767919
Alex Dresco <a.dresco@elsevier.com>
2019-06-09 15:13:23 +0100

    remove unused code

--- src/components/views/home.js
+++ src/components/views/home.js
@@ -86,13 +86,3 @@ const tileData = [
 }
 ]
 
-const styles = theme => ({
-root: {
-  flexGrow: 1,
-},
-paper: {
-  padding: theme.spacing.unit * 2,
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-},
-});
